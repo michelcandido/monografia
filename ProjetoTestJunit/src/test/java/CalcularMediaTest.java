fa6477d0b2a8361b1372347ec5ec6ba8ed9f92ad
michelcandido <michel@michelcandido.com.br>
2015-01-16 14:14:27 -0200

    Commit inicial

---
+++ ProjetoTestJunit/src/test/java/CalcularMediaTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+public class CalcularMediaTest {
+	
+	public void validarNota() {
+		
+	}
+	
+	public void validarMedia() {
+		
+	}
+	
+	public void validarValorMedia() {
+		
+	}
+	
+	public void validarStatus () {
+		
+	}
+	
+}
---
+++ ProjetoTestJunit/src/test/java/NotaTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+/**
+ * 
+ * @author Michel Candido
+ *
+ */
+public class NotaTest {
+	@Test
+	public void validarNotaIncorreta() {
+		
+	}
+	
+	@Test
+	public void validarValorValidoNota() {
+		
+	}
+	
+	@Test
+	public void validarExecaoDaNota() {
+		
+	}
+	
+	
+}
